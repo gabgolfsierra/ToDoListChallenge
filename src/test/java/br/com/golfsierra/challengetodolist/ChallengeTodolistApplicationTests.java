45a73148f9e71c3a2d008f9c50e50d3916726586
Gabriel Santino <ggs2209@gmail.com>
2023-08-02 23:55:11 -0300

    First Commit

---
+++ src/test/java/br/com/golfsierra/challengetodolist/ChallengeTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.golfsierra.challengetodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
